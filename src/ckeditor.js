e71214300b472c4bc2653ac196a34bb9cfa4f7af
Hovhannes Hovsepyan <hovhanneshovsepyan300@gmail.com>
2020-06-26 21:20:28 +0400

    added inline editor

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -5,6 +5,7 @@
 
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import InlineEditorBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
@@ -31,10 +32,10 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import Font from '@ckeditor/ckeditor5-font/src/font';
 
-export default class ClassicEditor extends ClassicEditorBase {}
+class ClassicEditor extends ClassicEditorBase {}
+class InlineEditor extends InlineEditorBase {}
 
-// Plugins to include in the build.
-ClassicEditor.builtinPlugins = [
+const plugins = [
 	Essentials,
 	UploadAdapter,
 	Autoformat,
@@ -61,8 +62,10 @@ ClassicEditor.builtinPlugins = [
 	Font
 ];
 
-// Editor configuration.
-ClassicEditor.defaultConfig = {
+ClassicEditor.builtinPlugins = plugins;
+InlineEditor.builtinPlugins = plugins;
+
+const config = {
 	toolbar: {
 		items: [
 			'heading',
@@ -101,4 +104,12 @@ ClassicEditor.defaultConfig = {
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
+};
+
+ClassicEditor.defaultConfig = config;
+InlineEditor.defaultConfig = config;
+
+export default {
+	ClassicEditor,
+	InlineEditor
 };
